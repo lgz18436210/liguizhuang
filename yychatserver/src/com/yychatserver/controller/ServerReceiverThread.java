35e70cfe1abe743e03862d5aac8a9aa22eff68af
lgz18436210 <1715615211@qq.com>
2019-04-03 23:34:05 +0800

    提交文件

--- yychatserver/src/com/yychatserver/controller/ServerReceiverThread.java
+++ yychatserver/src/com/yychatserver/controller/ServerReceiverThread.java
@@ -2,26 +2,35 @@ package com.yychatserver.controller;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 import com.yychat.model.Message;
 
 public class ServerReceiverThread extends Thread{
 	Socket s;
-	public ServerReceiverThread(Socket s) {
+	public ServerReceiverThread(Socket s) {//s是发送者Socket对象
 		this.s=s;
 	}
 	
 	public void run() {
 		ObjectInputStream ois;
+		ObjectOutputStream oos;
 		Message mess;
-		try {
-			ois = new ObjectInputStream(s.getInputStream());
-			mess=(Message)ois.readObject();
-			System.out.println(mess.getSender()+"对"+mess.getReceiver()+"说："+mess.getContent());
-		} catch (IOException | ClassNotFoundException e) {
-			e.printStackTrace();
+		while (true) {
+			try {
+				ois = new ObjectInputStream(s.getInputStream());
+				mess=(Message)ois.readObject();//接受聊天信息
+				System.out.println(mess.getSender()+"对"+mess.getReceiver()+"说："+mess.getContent());
+				
+				Socket s1=(Socket)StartServer.hmSocket.get(mess.getReceiver());//得到了与接受者相对应的服务器Socket对象
+				oos=new ObjectOutputStream(s1.getOutputStream());
+				oos.writeObject(mess);//转发聊天信息
+			} catch (IOException | ClassNotFoundException e) {
+				e.printStackTrace();
+			}
 		}
+		
 	
 	}
 }
